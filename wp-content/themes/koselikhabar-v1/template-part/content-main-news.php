3a9896c3f04bbe38db9e95328ff149fccf63aea7
aayurt <aayurtshrestha@gmail.com>
2021-03-21 09:37:33 +0545

    ad main news

--- wp-content/themes/koselikhabar-v1/template-part/content-main-news.php
+++ wp-content/themes/koselikhabar-v1/template-part/content-main-news.php
@@ -117,7 +117,7 @@ if (have_posts()) :
             <div class="row">
                 <div class="col-md-12">
                     <div class="below-desh-news-ad">
-                        <?php the_ad(74); ?>
+                        <?php the_ad(409); ?>
                     </div>
                 </div>
             </div>
