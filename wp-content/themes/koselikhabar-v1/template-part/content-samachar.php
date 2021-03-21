6845ccd794f72259df8eb6c7e6603f9b47ff2e37
aayurt <aayurtshrestha@gmail.com>
2021-03-21 15:01:26 +0545

    kurakani changes

--- wp-content/themes/koselikhabar-v1/template-part/content-samachar.php
+++ wp-content/themes/koselikhabar-v1/template-part/content-samachar.php
@@ -93,7 +93,7 @@ if (have_posts()) :
                 global $wp_query;
                 $v = 1;
                 $deshNews = array(
-                    'post_type' =>  'samachar',
+                    'post_type' =>  'discussion-news',
                     'post_per_page' =>  4,
                     'order' =>  'desc',
                 );
