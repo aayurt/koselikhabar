6b1677e4a2f98f7b064c7fbfb2ea8105ae90b75c
aayurt <aayurtshrestha@gmail.com>
2021-03-21 10:30:38 +0545

    sulsuley

--- wp-content/themes/koselikhabar-v1/template-part/content-sulsuley.php
+++ wp-content/themes/koselikhabar-v1/template-part/content-sulsuley.php
@@ -9,28 +9,28 @@ $deshNews = array(
 );
 query_posts($deshNews);
 $count = $wp_query->found_posts;
-if(have_posts()):
+if (have_posts()) :
 ?>
 
-<section class="samachar">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="row theme_color">
-                    <div class="col-12">
-                    सुलसुले 
+    <section class="samachar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="row theme_color">
+                        <div class="col-12">
+                            सुलसुले
+                        </div>
                     </div>
-                </div>
-                <?php
-                    while(have_posts()) : the_post();
+                    <?php
+                    while (have_posts()) : the_post();
                         if ($v == '1') {
-                ?>
+                    ?>
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="main_news">
                                         <h5 class="news_title">
-                                            <a href="<?php echo the_permalink(); ?>" title="<?php echo get_the_title();?>">
-                                                <?php the_title()?>
+                                            <a href="<?php echo the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
+                                                <?php the_title() ?>
                                             </a>
                                         </h5>
                                         <div class="row">
@@ -43,10 +43,10 @@ if(have_posts()):
                                                 <div class="news_desc">
                                                     <?php
                                                     $content = strip_tags(get_the_content());
-                                                    if(has_excerpt( $post->ID )){
+                                                    if (has_excerpt($post->ID)) {
                                                         the_excerpt();
-                                                    } else{
-                                                        echo '<p>'.wp_trim_words($content,'50', '....').'</p>';
+                                                    } else {
+                                                        echo '<p>' . wp_trim_words($content, '50', '....') . '</p>';
                                                     }
                                                     ?>
                                                 </div>
@@ -55,60 +55,58 @@ if(have_posts()):
                                     </div>
                                 </div>
                             </div>
-                    <?php } ?>
+                        <?php } ?>
 
-                    <?php
-                        if($v == '2'){
+                        <?php
+                        if ($v == '2') {
                             echo '<div class="row main_side_news">';
                         }
-                        if($v > 1){?>
+                        if ($v > 1) { ?>
                             <div class="col-md-4">
                                 <div class="featured_image">
-                                    <img src="<?php echo the_post_thumbnail_url('medium'); ?>" alt="adfs"/>
+                                    <img src="<?php echo the_post_thumbnail_url('medium'); ?>" alt="adfs" />
                                 </div>
                                 <div class="news_detail">
                                     <h5 class="news_title">
-                                        <a href="<?php echo the_permalink(); ?>" title="<?php echo get_the_title();?>">
-                                            <?php the_title()?>
+                                        <a href="<?php echo the_permalink(); ?>" title="<?php echo get_the_title(); ?>">
+                                            <?php the_title() ?>
                                         </a>
                                     </h5>
                                 </div>
                             </div>
                         <?php }
 
-                        if (($v == 4 || $v == $count) && ($count>1)) {
+                        if (($v == 4 || $v == $count) && ($count > 1)) {
                             echo '</div>';
                         }
                         ?>
 
-                <?php
-                    $v++;
+                    <?php
+                        $v++;
                     endwhile;
-                ?>
+                    ?>
 
-            </div>
-            <div class="col-md-4">
-                <div class="ad-block">
-                    <?php the_ad_group(35);?>
                 </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-    <!-- Below Desh News Ad starts  -->
-    <section class="below-desh-news">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="below-desh-news-ad">
-                        <?php the_ad(74); ?>
+                <div class="col-md-4">
+                    <div class="ad-block">
+                        <?php the_ad_group(35); ?>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- Below Desh News Ad end   -->
+    <section class="below-samachar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="below-samachar-ad">
+                        <?php the_ad(421); ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php
-    endif
+endif
 ?>
