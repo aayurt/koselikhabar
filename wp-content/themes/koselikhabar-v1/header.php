7584d04c1f12128730539cb7f68ff1cc0fb59df0
aayurt <aayurtshrestha@gmail.com>
2021-08-02 23:21:30 +0545

    header changes

--- wp-content/themes/koselikhabar-v1/header.php
+++ wp-content/themes/koselikhabar-v1/header.php
@@ -93,23 +93,23 @@
         <section class="header-menu">
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
-                    </button>
+                    </button> -->
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
 
-                        <?php
-                        wp_nav_menu(
-                            array(
-                                'theme_location' => 'header-menu',
-                                'container' => '',
-                                'items_wrap' => '<ul class="navbar-nav mr-auto">%3$s</ul>',
-                                'walker' => new wp_bootstrap_navwalker,
-                            )
-                        );
-                        ?>
-                    </div>
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'header-menu',
+                            'container' => '',
+                            'items_wrap' => '<ul class="navbar-nav mr-auto">%3$s</ul>',
+                            'walker' => new wp_bootstrap_navwalker,
+                        )
+                    );
+                    ?>
+                    <!-- </div> -->
                 </nav>
             </div>
         </section>
