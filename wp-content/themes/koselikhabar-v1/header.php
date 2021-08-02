0a6117e7c12eebcf4fbff7d3f457efd8551f6214
aayurt <aayurtshrestha@gmail.com>
2021-08-02 23:24:57 +0545

    header changes

--- wp-content/themes/koselikhabar-v1/header.php
+++ wp-content/themes/koselikhabar-v1/header.php
@@ -90,7 +90,7 @@
         </section>
         <!--Top Header End    -->
         <!-- Menu Start  -->
-        <section class="header-menu">
+        <section>
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
